19b27bd00688ab882608bd2b6d05aca6297eb574
kim-duyen-frontend <ntduyen255@gmail.com>
2022-10-12 14:42:31 +0700

    design ui todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Typography, Input, Button, Space, Form } from 'antd';
+import { PlusOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import "./formtodo.scss";
+const { Text, Title } = Typography;
 
-function App() {
+const App = () => {
+  const initialState = [
+    {
+      id: uuidv4(),
+      name: "Kim Duyên"
+    }
+  ];
+  const onFinish = (values) => {
+    values.id = uuidv4();
+    console.log({ ...values });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="wrapper">
+          <div className="form">
+            <Title level={2}>To do list</Title>
+            <Form
+              layout="vertical"
+              autoComplete="off"
+              onFinish={onFinish}
+            >
+              <Form.Item
+                name="text"
+                label="Text"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your text!',
+                  },
+                ]}
+              >
+                <Input size="large" placeholder="enter text..." />
+              </Form.Item>
+              <Form.Item>
+                <Space>
+                  <Button type="primary" icon={<PlusOutlined />} htmlType="submit">
+                    Add
+                  </Button>
+                </Space>
+              </Form.Item>
+            </Form>
+            <div className="listUser">
+              <Text type="success">List User Current</Text>
+              {initialState.map((user) => (
+                <div className="item" key={user.id}>
+                  <div className="name">{user.name}</div>
+                  <div className="actions">
+                    <Space size="small">
+                      <Button type="primary" size="small" shape="circle" icon={<EditOutlined />} />
+                      <Button type="primary" danger size="small" shape="circle" icon={<DeleteOutlined />} />
+                    </Space>
+                  </div>
+                </div>
+              ))}
+              <p></p>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
